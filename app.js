00fad8251bd1a06afc7d3157e9c037aca944f863
HuskyCreek <juands1903@gmail.com>
2022-09-22 23:45:46 -0500

    Js

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 0){
+            $('header').addClass('headeronmove');
+        }else{
+            $('header').removeClass('headeronmove');
+        }
+    });
+});
