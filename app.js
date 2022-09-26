1f19a8fc7689a5abcf3ffa13a7ab3f606df4cd61
HuskyCreek <juands1903@gmail.com>
2022-09-26 01:36:22 -0500

    Alerta

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ $(document).ready(function () {
 const contenidopres = document.getElementById("presentacion");
 contenidopres.insertAdjacentHTML(
   "beforeend",
-  "<div class = 'op'><div class='code'></div><p>Aqui me presento!</p></div>"
+  "<div class = 'op'><div class='code'></div><p id = 'sobremi'>Me gusta aprender continuamente, tener retos que me exijan  y crecer profesionalmente. Esta imagen me representa ya que es algo que veo a diario, esto porque soy muy curioso desde pequeño y no he dejado de aprender acerca de esto, desde desarrollo web, sistemas operativos e incluso el manejo de la terminal</p></div>"
 );
 
 const contenidohabs = document.getElementById("habilidades");
 contenidohabs.insertAdjacentHTML(
   "beforeend",
-  "<div class = 'contenedor-tabla op'><table><tr><th>Tecnologia</th><th>Nivel</th></tr><tr><td>Python</td><td>7</td><td>Bueno</td></tr><tr><td>HTML5</td><td>8</td><td>Muy Bueno</td></tr><tr><td>CSS3</td><td>8</td><td>Muy Bueno</td></tr><tr><td>Java</td><td>5</td><td>Aceptable</td></tr><tr><td>PostgreSQL</td><td>6</td><td>Bueno</td></tr><tr><td>C++</td><td>5</td><td>Aceptable</td></tr><tr><td>ReactJs</td><td>6</td><td>Bueno</td></tr></table></div>"
+  "<div class = 'contenedor-tabla op'><table><tr><th>Tecnologia</th><th>Nivel</th></tr><tr><td>Python</td><td>7</td><td>Bueno</td></tr><tr><td>HTML5</td><td>8</td><td>Muy Bueno</td></tr><tr><td>CSS3</td><td>8</td><td>Muy Bueno</td></tr><tr><td>Java</td><td>5</td><td>Aceptable</td></tr><tr><td>PostgreSQL</td><td>6</td><td>Bueno</td></tr><tr><td>C++</td><td>5</td><td>Aceptable</td></tr><tr><td>ReactJs</td><td>6</td><td>Bueno</td></tr><tr><td>Git</td><td>8</td><td>Muy Bueno</td></tr></table></div>"
 );
 
 const contenidoexp = document.getElementById("experiencia");
 contenidoexp.insertAdjacentHTML(
   "beforeend",
-  "<div class = 'op'><p>Tengo experiencia en!</p></div>"
+  "<div class = 'op'><p>Quiero seguir aprendiendo y fortalecer los conocimientos previamente adquiridos</p></div>"
 );
 
 const contenidocon = document.getElementById("contacto");
@@ -31,3 +31,23 @@ contenidocon.insertAdjacentHTML(
   "beforeend",
   "<div class = 'conteiner op'><form action='' class='form'><div class='input-container'><input type='text' id='asunto' class='text-input' autocomplete='off' placeholder='Asunto' required><label for='asunto' class='label'>Asunto</label></div><br><div class='input-container'><input type='email' id='email' class='text-input' autocomplete = 'off' placeholder = 'E-mail' required><label for='email' class='label'>E-mail</label></div><br><div class='input-container'><input type='text' id='message' class='text-input' placeholder='Escriba su Mensaje'autocomplete='off' maxlength='1000' required><label for='message' class='label'>Escriba su Mensaje</label></div><br><button id = 'alerta'><span>Enviar</span></button></form></div>"
 );
+
+function enviar() {
+  if (
+    document.getElementById("asunto").value != "" &&
+    document.getElementById("email").value != "" &&
+    document.getElementById("message").value != ""
+  ) {
+    alert(
+      "Asunto: " +
+        document.getElementById("asunto").value +
+        "\nCorreo: " +
+        document.getElementById("email").value +
+        "\nMensaje: " +
+        document.getElementById("message").value
+    );
+  }
+}
+
+const mensaje = document.getElementById("alerta");
+mensaje.addEventListener("click", enviar, true);
